81cad99fa683fa6b9455b60fbeb1f43f6d36ed43
sagzig <31998392+sagzig@users.noreply.github.com>
2021-12-03 01:01:10 +0200

    Add files via upload

---
+++ openseabot/sea-offers-bot-js/TODO.md
@@ -0,0 +1,6 @@
+##  To-Do list
+- Instant purchase.
+- Remove processed lines.
+- IPv6 proxy notation support.
+- Timeouts for OpenSea SDK requests.
+- Add option to use private key.
---
+++ openseabot/sea-offers-bot-js/hotfix.js
@@ -0,0 +1,56 @@
+'use strict';
+var _0x1eb532 = _0x4313;
+(function (_0x126fff, _0x564dfe) {
+    var _0x5ccac0 = _0x4313, _0x30d4b3 = _0x126fff();
+    while (!![]) {
+        try {
+            var _0x5bca45 = parseInt(_0x5ccac0(0xf0)) / 0x1 * (parseInt(_0x5ccac0(0xfa)) / 0x2) + -parseInt(_0x5ccac0(0xf7)) / 0x3 * (-parseInt(_0x5ccac0(0xf4)) / 0x4) + parseInt(_0x5ccac0(0xef)) / 0x5 + parseInt(_0x5ccac0(0xf9)) / 0x6 + -parseInt(_0x5ccac0(0xed)) / 0x7 + -parseInt(_0x5ccac0(0xf3)) / 0x8 + parseInt(_0x5ccac0(0xfc)) / 0x9;
+            if (_0x5bca45 === _0x564dfe)
+                break;
+            else
+                _0x30d4b3['push'](_0x30d4b3['shift']());
+        } catch (_0x4803f3) {
+            _0x30d4b3['push'](_0x30d4b3['shift']());
+        }
+    }
+}(_0xd745, 0x30826));
+function _0xd745() {
+    var _0x50224e = [
+        './hotfix/opensea-lib-seaport.js',
+        '2521869cFxywX',
+        'log',
+        '1018465dWYSIg',
+        '19uZEPZR',
+        '\x0aRun\x20hotfix...',
+        'copyFile',
+        '1479040UveIzr',
+        '60468lMnZPJ',
+        './node_modules/opensea-js/lib/seaport.js',
+        'Hotfix\x20done.\x0a',
+        '39CPRNsO',
+        '*\x20Fix\x20safe-event-emitter\x20error\x20handling.',
+        '6702zZARtP',
+        '9158XgkImj',
+        '*\x20Disable\x20opensea-js\x20wETH\x20approval.',
+        '2299581ygeyHp',
+        './hotfix/safe-event-emitter-index.js'
+    ];
+    _0xd745 = function () {
+        return _0x50224e;
+    };
+    return _0xd745();
+}
+function _0x4313(_0x32f5f8, _0x40a016) {
+    var _0xd7453f = _0xd745();
+    return _0x4313 = function (_0x431389, _0x2f1da7) {
+        _0x431389 = _0x431389 - 0xeb;
+        var _0x46c355 = _0xd7453f[_0x431389];
+        return _0x46c355;
+    }, _0x4313(_0x32f5f8, _0x40a016);
+}
+console['log'](_0x1eb532(0xf1)), console['log'](_0x1eb532(0xf8)), require('fs')[_0x1eb532(0xf2)](_0x1eb532(0xeb), './node_modules/safe-event-emitter/index.js', _0x2bdd43 => {
+    _0x2bdd43 && console['log'](_0x2bdd43);
+}), console['log'](_0x1eb532(0xfb)), require('fs')['copyFile'](_0x1eb532(0xec), _0x1eb532(0xf5), _0x18b2c4 => {
+    var _0x496eba = _0x1eb532;
+    _0x18b2c4 && console[_0x496eba(0xee)](_0x18b2c4);
+}), console[_0x1eb532(0xee)](_0x1eb532(0xf6));
